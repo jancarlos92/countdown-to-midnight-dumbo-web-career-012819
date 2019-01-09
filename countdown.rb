9772d2bc417b35d370f2b7549bdbad1cc7045172
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-09 14:25:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,10 @@
 #write your code here
 
-def countdown
-  
+def countdown(int)
+  while int >= 0 
+  puts " #{int} seconds left!"
+  int -= 1 
+  break if int == 0 
+  puts "happy new year"
+end
 end
